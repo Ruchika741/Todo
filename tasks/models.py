389724c0b54806e2e52c6bb977317bc58a8ce37f
Ruchika741 <ruchikasinha93@gmail.com>
2021-03-02 13:34:15 +0530

    Added category field

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    CATEGORIES_CHOICES = (('Important', 'Important'),
+                  ('Optional', 'Optional'),
+                  ('Daily update', 'Daily update'),
+                  ('Weekly update', 'Weekly update'),
+                  )
+    category = models.CharField(max_length= 200, choices= CATEGORIES_CHOICES)
     title = models.CharField(max_length= 200)
     complete = models.BooleanField(default= False)
     created = models.DateTimeField(auto_now_add= True)
